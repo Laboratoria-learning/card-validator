2fefe33b91788ca93a451c6c46317d791e82b414
YaniraAB <arenazas2.2@hotmail.com>
2018-01-31 19:46:19 -0500

    añadiendo estructura básica de JSON

---
+++ js/data.js
@@ -0,0 +1,38 @@
+let data = [
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  },
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  },
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  },
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  },
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  },
+  {
+  "numCard": 4293875288227864,
+  "date": {month : Enero, year : 2025},
+  "cvv": 564,
+  "name": "Yanira Jazmin Arenazas Bueno",
+  }
+]
