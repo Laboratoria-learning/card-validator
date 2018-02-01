4b73818935443bb93da58c06d2cd3e8d387bbf33
YaniraAB <arenazas2.2@hotmail.com>
2018-01-31 20:06:28 -0500

    modificando JSON

--- js/data.js
+++ js/data.js
@@ -1,37 +1,37 @@
 let data = [
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   },
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   },
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   },
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   },
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   },
   {
   "numCard": 4293875288227864,
-  "date": {month : Enero, year : 2025},
+  "date": {month : 01, year : 2025},
   "cvv": 564,
   "name": "Yanira Jazmin Arenazas Bueno",
   }
