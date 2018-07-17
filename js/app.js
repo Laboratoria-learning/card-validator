4397b96719144802da73b9bbc2f0dc993ff3ee45
Lainix <linkelin8@gmail.com>
2018-07-17 15:59:05 -0500

    Se sube funcionalidad para validar la tarjeta de credito

--- js/app.js
+++ js/app.js
@@ -1,10 +1,37 @@
-const form = document.querySelector('form');
+const isValidCard = () => {
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (validateCardDetails(form)) {
-    console.log('datos válido... enviar...');
-  } else {
-    console.log('datos inválidos');
+  let creditNumber = document.getElementById('creditNumber').value
+  console.log(creditNumber);
+  let noValido = /\s/;
+  if (noValido.test(creditNumber)) {
+    creditNumber = prompt("Ingresa el numero de tarjeta sin spacios");
   }
-});
+  let creditNumberReverse = (creditNumber.split("")).reverse();
+  let pairNumbers = [];
+
+  for (let i = 0; i < creditNumberReverse.length; i++) {
+    if (i % 2 !== 0) {
+      let multiplication = creditNumberReverse[i] * 2;
+      if (multiplication >= 10) {
+        let sum = 0;
+        while (multiplication) {
+          sum += multiplication % 10;
+          multiplication = Math.floor(multiplication / 10)
+        }
+        pairNumbers.push(sum)
+      } else {
+        pairNumbers.push(multiplication);
+      }
+    } else
+      pairNumbers.push(parseInt(creditNumberReverse[i]));
+  }
+  let numberValidate = 0;
+  for (let j = 0; j < pairNumbers.length; j++) {
+    numberValidate += pairNumbers[j];
+  }
+  if (numberValidate % 10 === 0) {
+    document.getElementById("resul").innerHTML = "Numero de tarjeta: " + creditNumber + " es Valido ";
+  } else {
+    document.getElementById("resul").innerHTML = "Numero de tarjeta: " + creditNumber + " es Invalido ";
+  }
+}
